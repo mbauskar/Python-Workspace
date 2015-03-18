f84eff01c1aab22e889fda3f43eef9b0e395f5c4
Makarand <mbauskar@gmail.com>
2015-03-18 13:07:01 +0530

    Program to swap two numbers

---
+++ swap.py
@@ -0,0 +1,10 @@
+# Program to swap to numbers
+
+a = 10
+b = 20
+
+print "before swapping - a = ", a, ", b : ", b
+
+a, b = b, a				# swaping values
+
+print "after swapping  - a = ", a, ", b : ", b
