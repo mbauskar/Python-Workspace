e87382e61d00d760fdd7b172c41bcdcc1a0fc755
Makarand <mbauskar@gmail.com>
2015-03-18 12:48:01 +0530

    Program for sting operation

--- stringOperations.py
+++ stringOperations.py
@@ -43,10 +43,42 @@ def addCommasToNumericString(numStr):
 	else:
 		return numStr
 
+def addCommasToNumericString(numStr, format):
+	# Function to add commas in numeric string using format
+	sufix = []
+
+	if numStr.find(".") != -1:
+		inputList =  list(numStr.split(".")[0])		# if numeric string contains decimal point then
+		sufix = ['.'] + list(numStr.split(".")[1])	# we will store the string after decimal point in suffix
+	else:
+		inputList = list(numStr)
+
+	if len(inputList) > 3:
+		commaAfterCount = 0
+		if format == "INR":
+			sufix = [","] +	inputList[-3:]	+ sufix	# adding comma  before last three digits, 
+													# inputList[-3:] returns last three digits
+			inputList = inputList[:-3]				# updating inputList so we can add comma after each 2 digit
+			commaAfterCount = 2
+		
+		elif format == "USD":
+			commaAfterCount = 3
+			#or
+			#return "{:,}".format(int(numStr))
+
+		for i in range(len(inputList))[::-commaAfterCount][1:]:	
+			inputList.insert(i+1, ",")
+
+		inputList += sufix							# adding sufix i.e last 3 digits to inputList
+
+		return "".join(inputList)#+"."+numStr.split(".")[1]
+	else:
+		return numStr
+
 def listToString(inputList):
 	opStr = ""
 	for ch in inputList:
 		opStr += ch						# Appending the character to string
 	return opStr
 
-print addCommasToNumericString("89.00")
+print addCommasToNumericString("123456789.00","USD")
