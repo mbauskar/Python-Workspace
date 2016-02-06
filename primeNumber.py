d689586b7e3190ac3c7963fc5ce320bd63328305
mbauskar <mbauskar@gmail.com>
2016-02-06 14:53:16 +0530

    prime numbers

--- primeNumber.py
+++ primeNumber.py
@@ -1,20 +1,29 @@
 # find the all the possible prime number from the given integer number
 
-from itertools import permutations
+from itertools import permutations, count, islice
+from math import sqrt
+import time
 
 number = 3657
 strNumber = str(number)
 listNumbers = []
+primeNumbers = []
 
 def is_prime(a):
-    return all(a % i for i in xrange(2, a))
+	# return n > 1 and all(n%i for i in islice(count(2), int(sqrt(n)-1)))
+    # return all(a % i for i in xrange(2, a))
+    return all(a % i for i in xrange(2, a/2))
 
 for i in xrange(1, len(strNumber)+1):
 	_list = map(lambda n: int("".join(n)), permutations(strNumber, i))
 	listNumbers.extend(_list)
 
 listNumbers = list(set(listNumbers))
+listNumbers.sort()
 
 for n in listNumbers:
 	if is_prime(n):
-		print n,
+		primeNumbers.append(n)
+
+print "count : ", len(primeNumbers)
+print "Prime Numbers : ", primeNumbers
